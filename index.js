2ce006874fe3fc842355816747cb41f9a391e99e
William Tomé <william.tomenh@gmail.com>
2020-10-19 23:55:30 -0300

    adicionado validação dos dados nos campos.

--- index.js
+++ index.js
@@ -13,12 +13,19 @@ app.get('/', (req,res) => {
 
 app.get('/cotacao', (req, res) => {
   const { cotacao, quantidade } = req.query;
-  const conversao = convert.convert(cotacao, quantidade)
-  res.render('cotacao', {
-    cotacao,
-    quantidade,
-    conversao
-  })
+  if (cotacao && quantidade) {
+    const conversao = convert.convert(cotacao, quantidade)
+    res.render('cotacao', {
+      error: false,
+      cotacao: convert.toMoney(cotacao),
+      quantidade: convert.toMoney(quantidade),
+      conversao: convert.toMoney(conversao)
+    })  
+  } else {
+    res.render('cotacao', {
+      error: 'Valores inválidos!'
+    })
+  }
 })
 
 app.listen(3000)
