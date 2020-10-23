074bc4234abf088e04ec96db32f28b95401c849b
William Tomé <william.tomenh@gmail.com>
2020-10-22 21:18:34 -0300

    exibir o resultado da consulta de cotação para o campo correspondente na home.

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const convert = require('./lib/convert')
+const apiBCB = require('./lib/api-bcb')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req,res) => {
-  res.render('home')
+app.get('/', async(req,res) => {
+  const cotacao = await apiBCB.getCotacao()
+  res.render('home', {
+    cotacao
+  })
 })
 
 app.get('/cotacao', (req, res) => {
