9f32c16e0cae2f883586c630ae3c59c874bd71c6
William Tomé <william.tomenh@gmail.com>
2020-11-10 09:19:02 -0300

    removida conversão da cotação

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', async(req,res) => {
   const cotacao = await apiBCB.getCotacao()
   res.render('home', {
-    cotacao: convert.toMoney(cotacao)
+    cotacao: cotacao
   })
 })
 
